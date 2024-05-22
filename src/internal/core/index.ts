8c7462f4a745800499a63ecf0632df3647b3e22c
anatawa12 <anatawa12@icloud.com>
2024-05-22 21:24:24 +0900

    lint: kill any but still with loose type checking

--- src/internal/core/index.ts
+++ src/internal/core/index.ts
@@ -12,7 +12,11 @@ export type Failure = { success: false };
 
 export type Result<T> = Success<T> | Failure;
 
-export type ParserHandler<T> = (input: string, index: number, state: any) => Result<T>
+export interface StateBase {
+	trace?: boolean,
+}
+
+export type ParserHandler<T, S extends StateBase> = (input: string, index: number, state: S) => Result<T>
 
 export function success<T>(index: number, value: T): Success<T> {
 	return {
@@ -26,11 +30,11 @@ export function failure(): Failure {
 	return { success: false };
 }
 
-export class Parser<T> {
+export class Parser<T, S extends StateBase> {
 	public name?: string;
-	public handler: ParserHandler<T>;
+	public handler: ParserHandler<T, S>;
 
-	constructor(handler: ParserHandler<T>, name?: string) {
+	constructor(handler: ParserHandler<T, S>, name?: string) {
 		this.handler = (input, index, state) : Failure | Success<T> => {
 			if (state.trace && this.name != null) {
 				const pos = `${index}`;
@@ -50,7 +54,7 @@ export class Parser<T> {
 		this.name = name;
 	}
 
-	map<U>(fn: (value: T) => U): Parser<U> {
+	map<U>(fn: (value: T) => U): Parser<U, S> {
 		return new Parser((input, index, state) => {
 			const result = this.handler(input, index, state);
 			if (!result.success) {
@@ -60,7 +64,7 @@ export class Parser<T> {
 		});
 	}
 
-	text(): Parser<string> {
+	text(): Parser<string, S> {
 		return new Parser((input, index, state) => {
 			const result = this.handler(input, index, state);
 			if (!result.success) {
@@ -71,7 +75,7 @@ export class Parser<T> {
 		});
 	}
 
-	many(min: number): Parser<T[]> {
+	many(min: number): Parser<T[], S> {
 		return new Parser((input, index, state) => {
 			let result;
 			let latestIndex = index;
@@ -91,32 +95,32 @@ export class Parser<T> {
 		});
 	}
 
-	sep(separator: Parser<unknown>, min: number): Parser<T[]> {
+	sep(separator: Parser<unknown, S>, min: number): Parser<T[], S> {
 		if (min < 1) {
 			throw new Error('"min" must be a value greater than or equal to 1.');
 		}
 		return seq(
-			this,
+			this as Parser<T, S>,
 			seq(
 				separator,
-				this,
+				this as Parser<T, S>,
 			).select(1).many(min - 1),
 		).map(result => [result[0], ...result[1]]);
 	}
 
-	select<K extends keyof T>(key: K): Parser<T[K]> {
+	select<K extends keyof T>(key: K): Parser<T[K], S> {
 		return this.map(v => v[key]);
 	}
 
-	option(): Parser<T | null> {
+	option(): Parser<T | null, S> {
 		return alt([
-			this,
+			this as Parser<T, S>,
 			succeeded(null),
 		]);
 	}
 }
 
-export function str<T extends string>(value: T): Parser<T> {
+export function str<T extends string>(value: T): Parser<T, object> {
 	return new Parser((input, index, _state) => {
 		if ((input.length - index) < value.length) {
 			return failure();
@@ -128,7 +132,7 @@ export function str<T extends string>(value: T): Parser<T> {
 	});
 }
 
-export function regexp<T extends RegExp>(pattern: T): Parser<string> {
+export function regexp<T extends RegExp>(pattern: T): Parser<string, object> {
 	const re = RegExp(`^(?:${pattern.source})`, pattern.flags);
 	return new Parser((input, index, _state) => {
 		const text = input.slice(index);
@@ -140,23 +144,29 @@ export function regexp<T extends RegExp>(pattern: T): Parser<string> {
 	});
 }
 
-type ParsedType<T extends Parser<unknown>> = T extends Parser<infer U> ? U : never;
+type ParsedType<T extends Parser<unknown, never>> = T extends Parser<infer U, never> ? U : never;
 
 export type SeqParseResult<T extends unknown[]> =
 	T extends [] ? [] 
 		: T extends [infer F, ...infer R]
 		? (
-			F extends Parser<unknown> ? [ParsedType<F>, ...SeqParseResult<R>] : [unknown, ...SeqParseResult<R>]
+			F extends Parser<unknown, never> ? [ParsedType<F>, ...SeqParseResult<R>] : [unknown, ...SeqParseResult<R>]
 			)
 		: unknown[];
 
-export function seq<Parsers extends Parser<unknown>[]>(...parsers: Parsers): Parser<SeqParseResult<Parsers>> {
+export type CommonState<T extends unknown[]> =
+	T extends [] ? StateBase
+		: T extends [Parser<unknown, infer S>, ...infer R] ? S & CommonState<R>
+		: T extends Parser<unknown, infer S>[] ? S
+		: never;
+
+export function seq<Parsers extends Parser<unknown, never>[]>(...parsers: Parsers): Parser<SeqParseResult<Parsers>, CommonState<Parsers>> {
 	return new Parser((input, index, state) => {
 		let result;
 		let latestIndex = index;
 		const accum = [];
 		for (let i = 0; i < parsers.length; i++) {
-			result = parsers[i].handler(input, latestIndex, state);
+			result = parsers[i].handler(input, latestIndex, state as never);
 			if (!result.success) {
 				return result;
 			}
@@ -167,11 +177,11 @@ export function seq<Parsers extends Parser<unknown>[]>(...parsers: Parsers): Par
 	});
 }
 
-export function alt<Parsers extends Parser<unknown>[]>(parsers: Parsers): Parser<ParsedType<Parsers[number]>> {
-	return new Parser<ParsedType<Parsers[number]>>((input, index, state): Result<ParsedType<Parsers[number]>> => {
+export function alt<Parsers extends Parser<unknown, never>[]>(parsers: Parsers): Parser<ParsedType<Parsers[number]>, CommonState<Parsers>> {
+	return new Parser<ParsedType<Parsers[number]>, CommonState<Parsers>>((input, index, state): Result<ParsedType<Parsers[number]>> => {
 		for (let i = 0; i < parsers.length; i++) {
 			const parser: Parsers[number] = parsers[i];
-			const result = parser.handler(input, index, state);
+			const result = parser.handler(input, index, state as never);
 			if (result.success) {
 				return result as Result<ParsedType<Parsers[number]>>;
 			}
@@ -180,13 +190,13 @@ export function alt<Parsers extends Parser<unknown>[]>(parsers: Parsers): Parser
 	});
 }
 
-function succeeded<T>(value: T): Parser<T> {
+function succeeded<T>(value: T): Parser<T, StateBase> {
 	return new Parser((_input, index, _state) => {
 		return success(index, value);
 	});
 }
 
-export function notMatch(parser: Parser<unknown>): Parser<null> {
+export function notMatch<S extends StateBase>(parser: Parser<unknown, S>): Parser<null, S> {
 	return new Parser((input, index, state) => {
 		const result = parser.handler(input, index, state);
 		return !result.success
@@ -234,8 +244,8 @@ export const lineEnd = new Parser((input, index, state) => {
 	return failure();
 });
 
-export function lazy<T>(fn: () => Parser<T>): Parser<T> {
-	const parser: Parser<T> = new Parser((input, index, state) => {
+export function lazy<T, S extends StateBase>(fn: () => Parser<T, S>): Parser<T, S> {
+	const parser: Parser<T, S> = new Parser((input, index, state) => {
 		parser.handler = fn().handler;
 		return parser.handler(input, index, state);
 	});
@@ -246,10 +256,10 @@ export function lazy<T>(fn: () => Parser<T>): Parser<T> {
 //type SyntaxReturn<T> = T extends (rules: Record<string, Parser<any>>) => infer R ? R : never;
 //export function createLanguage2<T extends Record<string, Syntax<any>>>(syntaxes: T): { [K in keyof T]: SyntaxReturn<T[K]> } {
 
-type ParserTable<T> = { [K in keyof T]: Parser<T[K]> };
+type ParserTable<T> = { [K in keyof T]: Parser<T[K], StateBase> };
 
 // TODO: 関数の型宣言をいい感じにしたい
-export function createLanguage<T>(syntaxes: { [K in keyof T]: (r: ParserTable<T>) => Parser<T[K]> }): ParserTable<T> {
+export function createLanguage<T>(syntaxes: { [K in keyof T]: (r: ParserTable<T>) => Parser<T[K], never> }): ParserTable<T> {
 	// @ts-expect-error initializing object so type error here
 	const rules: ParserTable<T> = {};
 	for (const key of Object.keys(syntaxes) as (keyof T & string)[]) {
@@ -260,7 +270,7 @@ export function createLanguage<T>(syntaxes: { [K in keyof T]: (r: ParserTable<T>
 			}
 			parser.name = key;
 			return parser;
-		});
+		}) as Parser<T[keyof T & string], StateBase>;
 	}
 	return rules;
 }
--- src/internal/parser.ts
+++ src/internal/parser.ts
@@ -1,7 +1,6 @@
 import * as M from '..';
 import * as P from './core';
 import { mergeText } from './util';
-import { SeqParseResult } from './core';
 
 // NOTE:
 // tsdのテストでファイルを追加しているにも関わらず「@twemoji/parser/dist/lib/regex」の型定義ファイルがないとエラーが出るため、
@@ -17,13 +16,13 @@ const space = P.regexp(/[\u0020\u3000\t]/);
 const alphaAndNum = P.regexp(/[a-z0-9]/i);
 const newLine = P.alt([P.crlf, P.cr, P.lf]);
 
-function seqOrText<Parsers extends P.Parser<unknown>[]>(...parsers: Parsers): P.Parser<SeqParseResult<Parsers> | string> {
-	return new P.Parser<SeqParseResult<Parsers> | string>((input, index, state) => {
+function seqOrText<Parsers extends P.Parser<unknown, never>[]>(...parsers: Parsers): P.Parser<P.SeqParseResult<Parsers> | string, P.CommonState<Parsers>> {
+	return new P.Parser<P.SeqParseResult<Parsers> | string, P.CommonState<Parsers>>((input, index, state) => {
 		// TODO: typesafe implementation
 		const accum: unknown[] = [];
 		let latestIndex = index;
 		for (let i = 0 ; i < parsers.length; i++) {
-			const result = parsers[i].handler(input, latestIndex, state);
+			const result = parsers[i].handler(input, latestIndex, state as never);
 			if (!result.success) {
 				if (latestIndex === index) {
 					return P.failure();
@@ -34,29 +33,38 @@ function seqOrText<Parsers extends P.Parser<unknown>[]>(...parsers: Parsers): P.
 			accum.push(result.value);
 			latestIndex = result.index;
 		}
-		return P.success(latestIndex, accum as SeqParseResult<Parsers>);
+		return P.success(latestIndex, accum as P.SeqParseResult<Parsers>);
 	});
 }
 
-const notLinkLabel = new P.Parser((_input, index, state) => {
+interface LinkState extends P.StateBase {
+	linkLabel?: boolean;
+}
+
+const notLinkLabel = new P.Parser((_input, index, state: LinkState) => {
 	return (!state.linkLabel)
 		? P.success(index, null)
 		: P.failure();
 });
 
-const nestable = new P.Parser((_input, index, state) => {
+interface RecursiveState extends P.StateBase {
+	depth: number;
+	nestLimit: number;
+}
+
+const nestable = new P.Parser<null, RecursiveState>((_input, index, state) => {
 	return (state.depth < state.nestLimit)
 		? P.success(index, null)
 		: P.failure();
 });
 
-function nest<T>(parser: P.Parser<T>, fallback?: P.Parser<string>): P.Parser<T | string> {
+function nest<T, S extends P.StateBase>(parser: P.Parser<T, S>, fallback?: P.Parser<string, S>): P.Parser<T | string, S & RecursiveState> {
 	// nesting limited? -> No: specified parser, Yes: fallback parser (default = P.char)
-	const inner = P.alt([
+	const inner: P.Parser<string | T, S & RecursiveState> = P.alt([
 		P.seq(nestable, parser).select(1),
 		(fallback != null) ? fallback : P.char,
 	]);
-	return new P.Parser<T | string>((input, index, state) => {
+	return new P.Parser<T | string, S & RecursiveState>((input, index, state) => {
 		state.depth++;
 		const result = inner.handler(input, index, state);
 		state.depth--;
@@ -223,7 +231,7 @@ export const language = P.createLanguage<TypeTable>({
 	},
 
 	quote: r => {
-		const lines: P.Parser<string[]> = P.seq( 
+		const lines = P.seq( 
 			P.str('>'),
 			space.option(),
 			P.seq(P.notMatch(newLine), P.char).select(1).many(0).text(),
@@ -316,7 +324,7 @@ export const language = P.createLanguage<TypeTable>({
 		});
 	},
 
-	big: r => {
+	big: (r) => {
 		const mark = P.str('***');
 		return seqOrText(
 			mark,
@@ -483,7 +491,7 @@ export const language = P.createLanguage<TypeTable>({
 			}
 			return P.success(result.index, result.value);
 		});
-		const arg: P.Parser<ArgPair> = P.seq(
+		const arg = P.seq(
 			P.regexp(/[a-z0-9_]+/i),
 			P.seq(
 				P.str('='),
@@ -493,7 +501,7 @@ export const language = P.createLanguage<TypeTable>({
 			return {
 				k: result[0],
 				v: (result[1] != null) ? result[1] : true,
-			};
+			} as ArgPair;
 		});
 		const args = P.seq(
 			P.str('.'),
@@ -557,7 +565,7 @@ export const language = P.createLanguage<TypeTable>({
 				P.regexp(/[a-z0-9_.-]+/i),
 			).select(1).option(),
 		);
-		return new P.Parser<M.MfmMention | string>((input, index, state) => {
+		return new P.Parser<M.MfmMention | string, LinkState>((input, index, state) => {
 			let result;
 			result = parser.handler(input, index, state);
 			if (!result.success) {
@@ -619,7 +627,7 @@ export const language = P.createLanguage<TypeTable>({
 			P.notMatch(P.alt([P.regexp(/[ \u3000\t.,!?'"#:/[\]【】()「」（）<>]/), space, newLine])),
 			P.char,
 		).select(1);
-		const innerItem: P.Parser<unknown> = P.lazy(() => P.alt([
+		const innerItem: P.Parser<unknown, RecursiveState> = P.lazy(() => P.alt([
 			P.seq(
 				P.str('('), nest(innerItem, hashTagChar).many(0), P.str(')'),
 			),
@@ -672,7 +680,7 @@ export const language = P.createLanguage<TypeTable>({
 	},
 
 	link: r => {
-		const labelInline = new P.Parser((input, index, state) => {
+		const labelInline = new P.Parser((input, index, state: LinkState) => {
 			state.linkLabel = true;
 			const result = r.inline.handler(input, index, state);
 			state.linkLabel = false;
@@ -691,7 +699,7 @@ export const language = P.createLanguage<TypeTable>({
 			P.alt([r.urlAlt, r.url]),
 			P.str(')'),
 		);
-		return new P.Parser<M.MfmLink>((input, index, state) => {
+		return new P.Parser<M.MfmLink, RecursiveState>((input, index, state) => {
 			const result = parser.handler(input, index, state);
 			if (!result.success) {
 				return P.failure();
@@ -708,7 +716,7 @@ export const language = P.createLanguage<TypeTable>({
 
 	url: () => {
 		const urlChar = P.regexp(/[.,a-z0-9_/:%#@$&?!~=+-]/i);
-		const innerItem: P.Parser<unknown> = P.lazy(() => P.alt([
+		const innerItem: P.Parser<unknown, RecursiveState> = P.lazy(() => P.alt([
 			P.seq(
 				P.str('('), nest(innerItem, urlChar).many(0), P.str(')'),
 			),
@@ -722,7 +730,7 @@ export const language = P.createLanguage<TypeTable>({
 			P.regexp(/https?:\/\//),
 			innerItem.many(1).text(),
 		);
-		return new P.Parser<M.MfmUrl | string>((input, index, state) => {
+		return new P.Parser<M.MfmUrl | string, RecursiveState>((input, index, state) => {
 			let result;
 			result = parser.handler(input, index, state);
 			if (!result.success) {
